3a97e0760a1c5b4a3cfeae21e17103be34ed60f5
tazleef <tajwar.aleef@gmail.com>
2016-12-08 22:01:42 +0100

    Final lab6/7

--- Lab6-7/Lab6_7.m
+++ Lab6-7/Lab6_7.m
@@ -101,6 +101,111 @@ title('Low-pass Chebyshev-I filter with order 20');
 %filter increasing with the order number
 
 
+%Question 2.1
+%Calling dirac function previously defined in previous labs
+dirac=dirac(40, 20); %dirac with n=20 and N=40
+figure();   
+plot(dirac); 
+title('Dirac Function'); xlabel('k'); ylabel('x(k)');
 
+%Setting value of scaling
+scale = 0.5; 
+Ts = 1; 
+alpha = scale*Ts;  
+alpha_signal = exp(-alpha) ;
+
+%Anti-Causal part of smoothing filter
+dirac_anticausal_smooth = zeros(length(dirac),1);
+dirac_length=length(dirac)-2:-1:1;
+for i =  dirac_length 
+ dirac_anticausal_smooth(i) = scale*alpha*alpha_signal*dirac(i+1)+(2*alpha_signal)*dirac_anticausal_smooth(i+1)-(alpha_signal^2)*dirac_anticausal_smooth(i+2);
+end
+figure()
+stem (dirac_anticausal_smooth) ;
+title('Anti-causal Smoothing'); 
+
+%Causal part of smoothing
+dirac_causal_smooth = zeros(length(dirac),1);
+for i = 3:length(dirac);
+ dirac_causal_smooth(i) = -scale*alpha*alpha_signal*dirac(i-1)+(2*alpha_signal)*dirac_anticausal_smooth(i-1)-(alpha_signal^2)*dirac_anticausal_smooth(i-2);
+end    
+figure()
+stem (dirac_causal_smooth) ;
+title('Causal Smmothing'); 
+
+%Step input Signal
+step10=step(40,10)
+step30=step(40,30)
+step=step10-step30;
+figure()
+stem(step)
+%Causal Derivative function
+step_causal = zeros(length (step),1) ;
+for i = 3 : length(step)
+ step_causal(i) = step(i)+alpha_signal*(alpha-1)*step(i-1)+(2*alpha_signal)*step_causal(i-1)-(alpha_signal^2)*step_causal(i-2) ;
+end
+figure()
+stem (step_causal) ;
+title('Causal Deravative');
+
+%Anti-causal derivative function
+step_anticausal = zeros(length (step),1);
+step_length = length(step)-2 : -1 : 1 ; 
+for i = step_length  
+ step_anticausal(i) = alpha_signal*(alpha+1)*step(i+1)-(alpha_signal^2)*step(i+2)+(2*alpha_signal)*step_anticausal(i+1)-(alpha_signal^2)*step_anticausal(i+2) ;  
+end
+figure()
+stem (step_anticausal);
+title('Anti-Causal Deravative');
+
+%Question 3
+barbara = imread('C:\Users\Tajwar Abrar Aleef\Documents\GitHub\DSP-TP-1617\Lab6-7\images\barbara.gif');
+figure();
+imshow(barbara);  
+case1 = zeros(size(barbara)); 
+case2 = zeros(size(barbara)); 
+
+
+for i = 1:size(barbara, 2)
+    image1 = barbara(:,i);
+    
+    response_causal = zeros(length (image1),1) ;
+    for i = 3 : length(image1)
+     response_causal(i) = image1(i)+alpha_signal*(alpha-1)*image1(i - 1)+(2*alpha_signal)*response_causal(i-1)-(alpha_signal^2)*response_causal(i-2) ;
+    end
+    
+    response_antiCausal = zeros(length (image1 ),1) ;
+    barbara_length = length(image1)-2 : -1 : 1 ;
+    for i =  barbara_length
+     response_antiCausal(i) = alpha_signal*(alpha+1)*image1(i+1)-(alpha_signal^2)*image1(i+2)+(2*alpha_signal)*response_antiCausal(i+1)-(alpha_signal^2)*response_antiCausal(i+2) ;
+    end
+    
+    response = response_causal + response_antiCausal;
+    
+    case1(:,i) = response;            
+end
+figure();
+imshow (case1, []);
+
+for i = 1:size(barbara, 2)
+
+    image2 = barbara(i,:);
+    
+    response_causal = zeros(length (image2),1) ;
+    for i = 3 : length(image2)
+     response_causal(i) = image2(i)+alpha_signal*(alpha-1)*image2(i - 1)+(2*alpha_signal)*response_causal(i-1)-(alpha_signal^2)*response_causal(i-2) ;
+    end
+    
+    response_antiCausal = zeros(length (image2 ),1) ;
+    barbara_length = length(image2)-2 : -1 : 1 ;
+    for i =  barbara_length
+     response_antiCausal(i) = alpha_signal*(alpha+1)*image2(i+1)-(alpha_signal^2)*image2(i+2)+(2*alpha_signal)*response_antiCausal(i+1)-(alpha_signal^2)*response_antiCausal(i+2) ;
+    end
+    response2 = response_causal + response_antiCausal;
+    
+    case2(i,:) = response2;   
+end
+figure();
+imshow (case2, []);
 
 
